d25ff08bbfcb626075b85e0ad1c5b7e68934942d
Sofonias Ayele <sof16ayele@gmail.com>
2022-10-17 22:41:40 +0000

    Method 2 task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -2,29 +2,27 @@
 """ Script that uses JSONPlaceholder API to get information about employee """
 import json
 import requests
-import sys
 
 
-if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
+def todojson():
+    """ function that gets todo tasks and exports data to json file """
+    ur = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    tr = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    users_data = {}
+    for i in ur:
+        userid = i.get('id')
+        username = i.get('username')
+        todos = list(filter(lambda x: x.get('userId') == userid, tr))
+        user_data = list(map(
+            lambda x: {
+                'username': username,
+                'task': x.get('title'),
+                'completed': x.get('completed')
+            }, todos))
+        users_data['{}'.format(userid)] = user_data
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(users_data, f)
 
-    userid = sys.argv[1]
-    user = '{}users/{}'.format(url, userid)
-    res = requests.get(user)
-    json_o = res.json()
-    name = json_o.get('username')
 
-    todos = '{}todos?userId={}'.format(url, userid)
-    res = requests.get(todos)
-    tasks = res.json()
-    l_task = []
-    for task in tasks:
-        dict_task = {"task": task.get('title'),
-                     "completed": task.get('completed'),
-                     "username": name}
-        l_task.append(dict_task)
-
-    d_task = {str(userid): l_task}
-    filename = '{}.json'.format(userid)
-    with open(filename, mode='w') as f:
-        json.dump(d_task, f)
+if __name__ == '__main__':
+    todojson()
